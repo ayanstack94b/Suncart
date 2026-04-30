32c0c24553575b9cd05c6fb7cd6a028dd7487e73
unknown <ayondevs.94b@gmail.com>
2026-04-30 17:28:45 +0530

    customised navbar with acticve pathname added lottie animation

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import { notFound } from 'next/navigation';
+import products from "@/data/products.json";
+import Image from 'next/image';
+import { Star } from 'lucide-react';
 
-const ProductDetailsPage = () => {
+const ProductDetailsPage = async ({ params }) => {
+
+    const {id} = await params;
+    const product = products.find(p => p.id === parseInt(id));
+    if (!product) {
+        notFound();
+    }
+
+    const { name, image, price, rating, description, brand, stock } = product;
+
     return (
-        <div>
-            ProductDetailsPage
+        <div className="max-w-6xl mx-auto px-6 py-10">
+
+            <div className="grid md:grid-cols-2 gap-10">
+
+                <Image
+                    src={image}
+                    alt={name}
+                    height={600}
+                    width={400}
+                    className="w-full h-80 object-cover rounded-lg"
+                />
+
+                <div>
+                    <h1 className="text-3xl font-bold mb-3">{name}</h1>
+
+                    <p className="text-gray-500 mb-2">Brand: {brand}</p>
+
+                    <p className="text-xl font-semibold mb-2">₹{price}</p>
+
+                    <p className="flex items-center gap-1 text-sm text-gray-500 mb-4">
+                        <Star
+                            size={18}
+                            className="text-yellow-500 fill-yellow-500 "
+                        />
+                        {rating}
+                    </p>
+
+                    <p className="mb-6">{description}</p>
+
+                    <p className="text-sm mb-6">
+                        Stock: {stock > 0 ? "Available" : "Out of stock"}
+                    </p>
+
+                    <button className="btn bg-orange-500 hover:bg-orange-600 text-white border-none">
+                        Buy Now
+                    </button>
+                </div>
+
+            </div>
+
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,51 @@
 "use client";
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from "next/navigation";
+import Lottie from "lottie-react";
+import animationData from "@/lottie/summerCart.json";
+
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const isLogin = pathname === "/login";
+    const isRegister = pathname === "/register";
 
     const links = <>
-        <li><Link className='font-semibold text-lg hover:text-orange-600' href={'/'}>Home</Link></li>
-        <li><Link className='font-semibold text-lg hover:text-orange-600' href={'/products'}>Products</Link></li>
-        <li><Link className='font-semibold text-lg hover:text-orange-600' href={'/my-profile'}>My Profile</Link></li>
-    </>
+        <li>
+            <Link
+                href="/"
+                className={`font-semibold text-lg ${pathname === "/" ? "text-orange-500" : "hover:text-orange-600"
+                    }`}
+            >
+                Home
+            </Link>
+        </li>
+
+        <li>
+            <Link
+                href="/products"
+                className={`font-semibold text-lg ${pathname.startsWith("/products")
+                        ? "text-orange-500"
+                        : "hover:text-orange-600"
+                    }`}
+            >
+                Products
+            </Link>
+        </li>
+
+        <li>
+            <Link
+                href="/my-profile"
+                className={`font-semibold text-lg ${pathname === "/my-profile"
+                        ? "text-orange-500"
+                        : "hover:text-orange-600"
+                    }`}
+            >
+                My Profile
+            </Link>
+        </li>
+    </>;
+
 
     return (
         <div className=''>
@@ -24,25 +61,45 @@ const Navbar = () => {
                             {links}
                         </ul>
                     </div>
-                    <Link href="/" className=" text-xl text-orange-500 hover:text-orange-600 font-bold">
-                        SunCart
-                    </Link>
+                    <div className="flex items-center gap-2">
+                        <Lottie animationData={animationData} className="w-10" />
+                        <span className="text-orange-500 font-bold text-xl">SunCart</span>
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {links}
                     </ul>
                 </div>
-                <div className="navbar-end gap-4">
+
+                {/* active inactive button */}
+                
+                <div className="navbar-end gap-2">
+
                     <Link href="/login">
-                        <span className=" font-bold text-lg hover:text-orange-600">Login</span>
+                        <button
+                            className={`btn ${isLogin
+                                    ? "bg-orange-500 text-white border-none"
+                                    : "btn-ghost"
+                                }`}
+                        >
+                            Login
+                        </button>
                     </Link>
+
                     <Link href="/register">
-                        <button className="btn bg-orange-500 hover:bg-orange-600 text-white border-none">
+                        <button
+                            className={`btn ${isRegister
+                                    ? "bg-orange-500 text-white border-none"
+                                    : "btn-ghost"
+                                }`}
+                        >
                             Register
                         </button>
                     </Link>
+
                 </div>
+
             </div>
         </div>
     );
