5d57ebb977c9ec8cd6a98c2579ab0f6b862b4596
unknown <ayondevs.94b@gmail.com>
2026-04-30 16:50:55 +0530

    added products cards to handle btn redirected to details page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import products from "@/data/products.json";
+import ProductCard from '@/components/ProductCard';
+
+
 
 const ProductsPage = () => {
     return (
-        <div className=''>
+        <div className='container mx-auto my-10 min-h-screen p-3 '>
             {/* Title */}
             <h1 className="text-2xl font-bold mb-8">
                 All Products
             </h1>
+            {/* grid section */}
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+
+                {
+                    products.map((product) => <ProductCard
+                        key={product.id}
+                        product={product}
+                    ></ProductCard>)
+                }
+
+            </div>
+
         </div>
     );
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,50 @@
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    
+    
+    
+    const { name, price, image, rating, id } = product;
+
     return (
-        <div>
-            Products card
+        <div className='container mx-auto'>
+            <div className="card bg-base-100  w-full shadow-sm">
+
+                {/* Image container */}
+                <figure className="relative w-full h-48">
+                    <Image
+                        src={image}
+                        alt={name}
+                        fill
+                        className="object-cover rounded-t-lg"
+                    />
+                </figure>
+
+                {/* Body */}
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title line-clamp-2">{name}</h2>
+
+                    <p className="text-sm text-gray-500">⭐ {rating}</p>
+
+                    <p className="font-bold text-lg">₹{price}</p>
+
+                    <div className="card-actions">
+                       
+                           <Link href={`/products/${id}`}>
+                            <button className="btn bg-orange-500 hover:bg-orange-600 text-white border-none">
+                                View Details
+                            </button>
+                           </Link>
+                       
+                    </div>
+                </div>
+
+            </div>
+           
         </div>
     );
 };
